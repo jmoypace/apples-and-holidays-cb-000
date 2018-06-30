825b4145755ddfd7d9329accb213afcfee732ecd
jmoypace <jmoypace@gmail.com>
2018-06-30 00:44:37 +0000

    automatically backed up by learn

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -49,13 +49,10 @@ def all_supplies_in_holidays(holiday_hash)
   # Summer:
   #   Fourth Of July: Fireworks, BBQ
   # etc.
-holiday_hash.each do|season|
-  season.capitalize
-  season.each do|holiday, items|
-    holiday.capitalize
-    items.each do|items |
-      items.capitalize
-    end
+holiday_hash.each do|season, holidays|
+  puts "#{season.capitalize}"
+  holidays.each do|holiday, supplies|
+    puts" #{holiday.to_s.split('_').map {|w| w.capitalize }.join(' ') }: #{supplies.join(", ")}"
   end
 end
 end
